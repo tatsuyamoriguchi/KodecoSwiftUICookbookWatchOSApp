feacc2c259e212867726c5bd91acf322df5624f0
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-04 13:42:23 -0700

    iPad app

---
+++ watchOSApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchOSApp Watch App
+//
+//  Created by Tatsuya Moriguchi on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchOSApp Watch App/watchOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSAppApp.swift
+//  watchOSApp Watch App
+//
+//  Created by Tatsuya Moriguchi on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct watchOSApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
