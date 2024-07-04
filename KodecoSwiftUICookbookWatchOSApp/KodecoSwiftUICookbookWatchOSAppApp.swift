878e196b7b00b9c9888094c7799a3d0d60903e13
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-04 13:09:24 -0700

    Initial Commit

---
+++ KodecoSwiftUICookbookWatchOSApp/KodecoSwiftUICookbookWatchOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodecoSwiftUICookbookWatchOSAppApp.swift
+//  KodecoSwiftUICookbookWatchOSApp
+//
+//  Created by Tatsuya Moriguchi on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct KodecoSwiftUICookbookWatchOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
